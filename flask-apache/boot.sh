2b15a7aad561e7eeeee16b9be295e22acd150e72
Daniel J. B. Clarke <u8sand@gmail.com>
2019-08-30 12:39:16 -0400

    Removed old templates

--- flask-apache/boot.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-adduser --disabled-password --gecos '' r
-cd /flask-apache/
-mod_wsgi-express setup-server wsgi.py --port=80 --user r --group r --server-root=/etc/flask-apache --socket-timeout=600
-chmod a+x /etc/flask-apache/handler.wsgi
-/etc/flask-apache/apachectl start
-tail -f /etc/flask-apache/error_log
--- flask-nginx/app/app.py
+++
@@ -1,8 +0,0 @@
-import flask
-import os
-
-app = flask.Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def index():
-    return 'hello world'
