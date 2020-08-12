0f9242fa4edeeee65e685bb5b8e6641e33c674f9
erol <je.evangelista04@gmail.com>
2020-08-12 13:11:47 -0400

    Fixed errors in javascript-react-microbundle cookie cutter

--- javascript-react-microbundle/{{ cookiecutter.project_slug }}/public/demo.js
+++ javascript-react-microbundle/{{ cookiecutter.project_slug }}/public/demo.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {{ project_component }} from '../src/index'
+import {{ cookiecutter.project_component }} from '../src/index'
 
 export default class Demo extends React.Component {
   render = () => (
-    <{{ project_component }}
+    <{{ cookiecutter.project_component }}
       example="world"
     />
   )
