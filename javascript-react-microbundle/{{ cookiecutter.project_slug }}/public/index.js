41989a599ceb46d810404c1a81a7cad19fe4dd0c
Daniel Clarke <u8sand@gmail.com>
2023-03-09 18:14:15 -0500

    Remove deprecated javascript-react-microbundle

--- javascript-react-microbundle/{{ cookiecutter.project_slug }}/public/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Demo from './demo.js'
-
-const el = document.createElement(el)
-document.body.appendChild(el)
-
-ReactDOM.render(<Demo />, el)
